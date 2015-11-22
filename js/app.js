673bc50bf65978f1513a78329daa7ca12708bbd1
Benjamin Leeds <leeds@uw.edu>
2015-11-21 18:43:01 -0800

    update OAuth handling code

--- js/app.js
+++ js/app.js
@@ -30,3 +30,20 @@ function handleAuthClick(event) {
   gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
   return false;
 }
+
+ // Load the API and make an API call.  Display the results on the screen.
+function makeApiCall() {
+  gapi.client.load('plus', 'v1', function() {
+    var request = gapi.client.plus.people.get({
+      'userId': 'me'
+    });
+    request.execute(function(resp) {
+      var heading = document.createElement('h4');
+      var image = document.createElement('img');
+      image.src = resp.image.url;
+      heading.appendChild(image);
+      heading.appendChild(document.createTextNode(resp.displayName));
+      document.getElementById('content').appendChild(heading);
+    });
+  });
+}

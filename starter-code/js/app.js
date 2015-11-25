45bbe9e5bd133733fb8ff037fc66fdfc1dae4ed8
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2015-11-24 19:38:38 -0800

    starter code now has my completed version of the code

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -17,9 +17,9 @@ myApp.controller('GoogleApiCtrl', ['$scope', '$window', function($scope,$window)
   $scope.isBackendReady = false; 
 
   // OAuth2 clientID
-  var clientId = '282322657286-thqf8d9ol44fcasamt9e358p2ghigb1c.apps.googleusercontent.com'; 
+  var clientId = '353530661501-7v82qvc9ikqou9e5drvrjm0iq29pre2i.apps.googleusercontent.com'; 
   // general api access key
-  var apiKey = 'AIzaSyDbyRiJkjf4CU5YO6B6sc_K_iQkw-rXC74'; 
+  var apiKey = 'AIzaSyAHXtI167IuI_g4ZzzltLmxudua-Li6494'; 
 
   // list of scopes the user will be asked to grant access to separated by a space character (' ')
   var scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly']; 
@@ -31,23 +31,41 @@ myApp.controller('GoogleApiCtrl', ['$scope', '$window', function($scope,$window)
   // START
   // handleClientLoad function called when Google JS library is loaded in index.html head tag
   $scope.handleClientLoad = function() { 
-    
+    gapi.client.setApiKey(apiKey);
+    window.setTimeOut($scope.checkAuth, 1);
   }
 
   // check authorization status on page load and as needed in the future
   $scope.checkAuth = function() {
-    
-    
+    gapi.auth.authorize(
+      {
+        client_id: clientId, 
+        scope: scopes.join(' '), 
+        immediate: true 
+      }, $scope.handleAuthResult);
   }
 
   // handle authorization result
   $scope.handleAuthResult = function(authResult) {
-    
+    var authorizeButton = document.getElementById('authorize-button');
+
+    if (authResult && !authResult.error) {
+      authorizeButton.style.visibility = 'hidden';
+      $scope.loadDriveApi(); 
+    } else {
+      authorizeButton.style.visibility = '';
+    }
     
   }
    
   // authorize the user when the Sign In with Google button is pressed
   $scope.handleAuthClick = function(event) {
+      gapi.auth.authorize(
+      {
+        client_id: clientId,
+        scope: scopes,
+        immediate: false 
+      }, $scope.handleAuthResult);
 
     
   }
@@ -56,9 +74,9 @@ myApp.controller('GoogleApiCtrl', ['$scope', '$window', function($scope,$window)
   $scope.loadDriveApi = function(){
 
     // load the API
-   
-      // call listFiles()
-     
+    gapi.client.load('drive', 'v2', function() {
+
+    $scope.listFiles(); 
     })
   }
 
@@ -66,13 +84,19 @@ myApp.controller('GoogleApiCtrl', ['$scope', '$window', function($scope,$window)
   $scope.listFiles = function() {
 
     // build a request object to request up to ten Drive files
-    
+    var request = gapi.client.drive.files.list(
+      {
+        'maxResults': 10
+      }
+    );
 
     // execute the request
+    request.execute(function(resp) {
 
-    
-
- 
+      $scope.files = resp.items;
+      $scope.isBackendReady = true;
+     
+    });
   }
 
 }]);
